1cd4f578d9db8ef54094ceb5f9a92e30bcbe7e26
Luke Huisman <huisman.l@yahoo.com>
2024-02-07 14:12:15 +0100

    Added wysiqyg editor

--- app/public/index.php
+++ app/public/index.php
@@ -4,8 +4,17 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $router = new \Bramus\Router\Router();
 
-$router->get('/hello', function () {
-    echo 'Hello world!';
+$router->setNamespace('\Controllers');
+
+$router->post('/wysiwyg', function () {
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+});
+
+$router->get('/', function () {
+    $wysiqyg = new Services\Wysiqyg();
+    $wysiqyg->render("echo 'Hello World!';", "content", "/wysiwyg");
 });
 
 $router->run();
