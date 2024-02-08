8d35dd18845ca5f9e7939528e194bf393d71e04b
Yara Bok <yarabok210804@gmail.com>
2024-02-08 14:39:14 +0100

    header complete + yummy pages added

--- app/public/index.php
+++ app/public/index.php
@@ -1,5 +1,35 @@
 <?php
-session_start();
-require_once __DIR__ . '/../views/elements/header.php';
-require_once __DIR__ . '/../router/dynamicRouter.php';
-require_once __DIR__ . '/../views/elements/footer.php';
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$router = new \Bramus\Router\Router();
+
+$router->setNamespace('\Controllers');
+
+$router->post('/wysiwyg', function () {
+    echo '<pre>';
+    var_dump($_POST);
+    echo '</pre>';
+});
+
+$router->get('/', function () {
+    $home = new Views\Home();
+    $home->render();
+});
+
+$router->get('/music', function () {
+    $music = new Views\Music();
+    $music->render();
+});
+
+$router->get('/yummy', function () {
+    $yummy = new Controllers\YummyController();
+    $yummy->yummyOverview();
+});
+
+$router->get('/yummyDetail', function () {
+    $yummy = new Controllers\YummyController();
+    $yummy->yummyDetail();
+});
+
+$router->run();
