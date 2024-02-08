97c4e29a85f55065e15c43554765f20c280c7704
Luke Huisman <huisman.l@yahoo.com>
2024-02-08 10:18:44 +0100

    Added qr code service

--- app/vendor/composer/autoload_real.php
+++ app/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit2a5b000460c064a9c32fe6ac415cfb09
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
