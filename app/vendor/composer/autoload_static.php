c469d8aaabd07547544dfc5d300f7a7285ab230c
Luke Huisman <huisman.l@yahoo.com>
2024-02-08 10:18:44 +0100

    Added qr code service

--- app/vendor/composer/autoload_static.php
+++ app/vendor/composer/autoload_static.php
@@ -6,77 +6,184 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09
 {
-    public static $prefixLengthsPsr4 = array (
-        'V' => 
-        array (
+    public static $files = array(
+        '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
+        'a4a119a56e50fbb293281d9a48007e0e' => __DIR__ . '/..' . '/symfony/polyfill-php80/bootstrap.php',
+        '6e3fae29631ef280660b3cdad06f25a8' => __DIR__ . '/..' . '/symfony/deprecation-contracts/function.php',
+        '662a729f963d39afe703c9d9b7ab4a8c' => __DIR__ . '/..' . '/symfony/polyfill-php83/bootstrap.php',
+        '667aeda72477189d0494fecd327c3641' => __DIR__ . '/..' . '/symfony/var-dumper/Resources/functions/dump.php',
+    );
+
+    public static $prefixLengthsPsr4 = array(
+        'V' =>
+        array(
             'Views\\' => 6,
         ),
-        'S' => 
-        array (
+        'S' =>
+        array(
+            'Symfony\\Polyfill\\Php83\\' => 23,
+            'Symfony\\Polyfill\\Php80\\' => 23,
+            'Symfony\\Polyfill\\Mbstring\\' => 26,
+            'Symfony\\Polyfill\\Ctype\\' => 23,
+            'Symfony\\Contracts\\Translation\\' => 30,
+            'Symfony\\Contracts\\Service\\' => 26,
+            'Symfony\\Contracts\\EventDispatcher\\' => 34,
+            'Symfony\\Contracts\\Cache\\' => 24,
+            'Symfony\\Component\\Yaml\\' => 23,
+            'Symfony\\Component\\VarExporter\\' => 30,
+            'Symfony\\Component\\VarDumper\\' => 28,
+            'Symfony\\Component\\Routing\\' => 26,
+            'Symfony\\Component\\HttpKernel\\' => 29,
+            'Symfony\\Component\\HttpFoundation\\' => 33,
+            'Symfony\\Component\\Finder\\' => 25,
+            'Symfony\\Component\\Filesystem\\' => 29,
+            'Symfony\\Component\\EventDispatcher\\' => 34,
+            'Symfony\\Component\\ErrorHandler\\' => 31,
+            'Symfony\\Component\\DependencyInjection\\' => 38,
+            'Symfony\\Component\\Config\\' => 25,
+            'Symfony\\Component\\Cache\\' => 24,
+            'Symfony\\Bundle\\TwigBundle\\' => 26,
+            'Symfony\\Bundle\\FrameworkBundle\\' => 31,
+            'Symfony\\Bridge\\Twig\\' => 20,
             'Services\\' => 9,
         ),
-        'R' => 
-        array (
+        'R' =>
+        array(
             'Routers\\' => 8,
             'Repositories\\' => 13,
         ),
-        'M' => 
-        array (
+        'P' =>
+        array(
+            'Psr\\Log\\' => 8,
+            'Psr\\EventDispatcher\\' => 20,
+            'Psr\\Container\\' => 14,
+            'Psr\\Cache\\' => 10,
+        ),
+        'M' =>
+        array(
             'Models\\' => 7,
         ),
-        'F' => 
-        array (
+        'F' =>
+        array(
             'Firebase\\JWT\\' => 13,
         ),
-        'C' => 
-        array (
+        'E' =>
+        array(
+            'Endroid\\QrCode\\' => 15,
+            'Endroid\\QrCodeBundle\\' => 21,
+            'Endroid\\Installer\\' => 18,
+        ),
+        'D' =>
+        array(
+            'DASPRiD\\Enum\\' => 13,
+        ),
+        'C' =>
+        array(
             'Controllers\\' => 12,
         ),
+        'B' =>
+        array(
+            'BaconQrCode\\' => 12,
+        ),
     );
 
-    public static $prefixDirsPsr4 = array (
-        'Views\\' => 
-        array (
+    public static $prefixDirsPsr4 = array(
+        'Views\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/views',
         ),
-        'Services\\' => 
-        array (
+        'Services\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/services',
         ),
-        'Routers\\' => 
-        array (
+        'Routers\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/routers',
         ),
-        'Repositories\\' => 
-        array (
+        'Repositories\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/repositories',
         ),
-        'Models\\' => 
-        array (
+        'Psr\\Log\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/psr/log/src',
+        ),
+        'Psr\\EventDispatcher\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/psr/event-dispatcher/src',
+        ),
+        'Psr\\Container\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/psr/container/src',
+        ),
+        'Psr\\Cache\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/psr/cache/src',
+        ),
+        'Models\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/models',
         ),
-        'Firebase\\JWT\\' => 
-        array (
+        'Firebase\\JWT\\' =>
+        array(
             0 => __DIR__ . '/..' . '/firebase/php-jwt/src',
         ),
-        'Controllers\\' => 
-        array (
+        'Endroid\\QrCode\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/endroid/qr-code/src',
+        ),
+        'Endroid\\QrCodeBundle\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/endroid/qr-code-bundle/src',
+        ),
+        'Endroid\\Installer\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/endroid/installer/src',
+        ),
+        'DASPRiD\\Enum\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/dasprid/enum/src',
+        ),
+        'Controllers\\' =>
+        array(
             0 => __DIR__ . '/../..' . '/controllers',
+        ),
+        'BaconQrCode\\' =>
+        array(
+            0 => __DIR__ . '/..' . '/bacon/bacon-qr-code/src',
         ),
     );
 
-    public static $prefixesPsr0 = array (
-        'B' => 
-        array (
-            'Bramus' => 
-            array (
+    public static $prefixesPsr0 = array(
+        'B' =>
+        array(
+            'Bramus' =>
+            array(
                 0 => __DIR__ . '/..' . '/bramus/router/src',
             ),
         ),
     );
 
-    public static $classMap = array (
+    public static $classMap = array(
+        'Attribute' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Attribute.php',
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
+        'DateError' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateError.php',
+        'DateException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateException.php',
+        'DateInvalidOperationException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateInvalidOperationException.php',
+        'DateInvalidTimeZoneException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateInvalidTimeZoneException.php',
+        'DateMalformedIntervalStringException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateMalformedIntervalStringException.php',
+        'DateMalformedPeriodStringException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateMalformedPeriodStringException.php',
+        'DateMalformedStringException' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateMalformedStringException.php',
+        'DateObjectError' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateObjectError.php',
+        'DateRangeError' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/DateRangeError.php',
+        'Override' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/Override.php',
+        'PhpToken' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/PhpToken.php',
+        'SQLite3Exception' => __DIR__ . '/..' . '/symfony/polyfill-php83/Resources/stubs/SQLite3Exception.php',
+        'Stringable' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/Stringable.php',
+        'UnhandledMatchError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/UnhandledMatchError.php',
+        'ValueError' => __DIR__ . '/..' . '/symfony/polyfill-php80/Resources/stubs/ValueError.php',
+        '�' => __DIR__ . '/..' . '/symfony/cache/Traits/ValueWrapper.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
@@ -86,7 +193,6 @@ class ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09
             $loader->prefixDirsPsr4 = ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit2a5b000460c064a9c32fe6ac415cfb09::$classMap;
-
         }, null, ClassLoader::class);
     }
 }
