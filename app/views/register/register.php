902f7dca11deb6eb45cd85312a0b3b4db28e7c86
ThijsMoerland <Moerland8@gmail.com>
2024-02-10 16:49:41 +0100

    register user working with hashedPass

---
+++ app/views/register/register.php
@@ -0,0 +1,65 @@
+<div class="container">
+  <form class="my-3" action="/register/validateUser" method="POST">
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="firstName">First name</label>
+          <input type="text" class="form-control" name="firstName" id="firstName" placeholder="First name">
+        </div>
+      </div>
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="lastName">Last name</label>
+          <input type="text" class="form-control" name="lastName" id="lastName" placeholder="Last name">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="text" class="form-control" name="email" id="email" placeholder="Email">
+        </div>
+      </div>
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="dateOfBirth">Date of birth</label>
+          <input type="date" name="dateOfBirth" class="form-control" id="dateOfBirth">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="address">Address</label>
+          <input type="text" class="form-control" name="address" id="address" placeholder="Address">
+        </div>
+      </div>
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="phoneNumber">Phone number</label>
+          <input type="tel" class="form-control" name="phoneNumber" id="phoneNumber" placeholder="Phone number">
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+        </div>
+      </div>
+      <div class="col-md-6 mb-3">
+        <div class="form-group">
+          <label for="gender">Gender</label>
+          <select class="form-control" name="gender" id="gender">
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
