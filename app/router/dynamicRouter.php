cf7f5fc83e7ae3481838c65c3103703d4246d8b4
Luke Huisman <huisman.l@yahoo.com>
2024-02-12 09:54:33 +0100

    Update router and add 404 page

---
+++ app/router/dynamicRouter.php
@@ -0,0 +1,59 @@
+<?php
+// Require composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+// Define controller namespace
+$router->setNamespace('\Controllers');
+/**
+ * Handle dynamic routing based on the controller and action parameters.
+ *
+ * @param string $controller The name of the controller.
+ * @param string $action The name of the function called within the router.
+ * @return void
+ */
+$router->get('/{controller}/{action}', function ($controller, $action) {
+    $controller = ucfirst($controller); // Capitalize the first letter
+    $controller = "\\Controllers\\{$controller}Controller"; // Append 'Controller' to the controller name and prepend the namespace
+
+    if (class_exists($controller)) {
+        $controllerInstance = new $controller();
+        if (method_exists($controllerInstance, $action)) {
+            $controllerInstance->$action();
+        } else if (method_exists($controllerInstance, 'index')) {
+            $controllerInstance->index();
+        } else {
+            require_once __DIR__ . '/../views/404.php';
+        }
+    } else {
+        require_once __DIR__ . '/../views/404.php';
+    }
+});
+
+/**
+ * Handles dynamic routing for POST requests.
+ *
+ * @param string $controller The name of the controller.
+ * @param string $action The name of the action.
+ * @return void
+ */
+$router->post('/{controller}/{action}', function ($controller, $action) {
+    $controller = ucfirst($controller); // Capitalize the first letter
+    $controller = "\\Controllers\\{$controller}Controller"; // Append 'Controller' to the controller name and prepend the namespace
+
+    if (class_exists($controller)) {
+        $controllerInstance = new $controller();
+
+        if (method_exists($controllerInstance, $action)) {
+            $controllerInstance->$action();
+        } else {
+            echo "Action {$action} not found in controller {$controller}";
+        }
+    } else {
+        echo "Controller {$controller} not found";
+    }
+});
+
+$router->run();
