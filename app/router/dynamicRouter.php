f111c3bc66b0b98fd136dd4ed293d69e70bce1ad
ThijsMoerland <Moerland8@gmail.com>
2024-02-14 13:39:44 +0100

    KUT Router

--- app/router/dynamicRouter.php
+++ app/router/dynamicRouter.php
@@ -24,6 +24,17 @@ $router->get('/{controller}(/[a-z0-9_-]+)?', function ($controller, $action) {
     $controller = ucfirst($controller); // Capitalize the first letter
     $controller = "\\Controllers\\{$controller}Controller"; // Append 'Controller' to the controller name and prepend the namespace
 
+    $controller = explode('/', $controller);
+    if(count($controller) > 1){
+        //Luke je kut controller werkte niet als je naar /register/loginview ging zocht hij naar een controller genaamd register/loginviewController 
+        //maar hij moest zoeken naar een controller genaamd registerController en dan de functie loginview wat hij nu doet
+        $action = $controller[1];
+        $action = str_replace('Controller', '', $action);
+        $controller = $controller[0].'Controller';
+    } else {
+        $controller = $controller[0];
+    }
+    
     if (class_exists($controller)) {
         $controllerInstance = new $controller();
         if ($action !== null && method_exists($controllerInstance, $action)) {
