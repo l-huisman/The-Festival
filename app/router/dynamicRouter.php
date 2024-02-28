3bffc7b756f26b30aabc31a2dc71ba7fc74b1029
Luke Huisman <huisman.l@yahoo.com>
2024-02-28 09:03:34 +0100

    Fix controller name and handle GET parameter correctly

--- app/router/dynamicRouter.php
+++ app/router/dynamicRouter.php
@@ -25,21 +25,21 @@ $router->get('/{controller}(/[a-z0-9_-]+)?', function ($controller, $action) {
     $controller = "\\Controllers\\{$controller}Controller"; // Append 'Controller' to the controller name and prepend the namespace
 
     $controller = explode('/', $controller);
-    if(count($controller) > 1){
+    if (count($controller) > 1) {
         //Luke je kut controller werkte niet als je naar /register/loginview ging zocht hij naar een controller genaamd register/loginviewController 
         //maar hij moest zoeken naar een controller genaamd registerController en dan de functie loginview wat hij nu doet
         $action = $controller[1];
         $action = str_replace('Controller', '', $action);
-        $controller = $controller[0].'Controller';
+        $controller = $controller[0] . 'Controller';
     } else {
         $controller = $controller[0];
     }
-    
+
     if (class_exists($controller)) {
         $controllerInstance = new $controller();
         if ($action !== null && method_exists($controllerInstance, $action)) {
-            if (isset($_GET['value'])) {
-                $value = $_GET['value'];
+            if (isset($_GET['id'])) {
+                $value = $_GET['id'];
                 $controllerInstance->$action($value);
             } else {
                 $controllerInstance->$action();
