1c356bef287f686e2ee17f2abc97126109dc038c
l-huisman <64755533+l-huisman@users.noreply.github.com>
2024-03-12 20:48:38 +0100

    Add delete functionality to CustomController

--- app/views/wysiwyg/index.php
+++ app/views/wysiwyg/index.php
@@ -1,10 +1,11 @@
 <script src="https://cdn.tiny.cloud/1/30x8vgbwun6m4b3yb3zv85ytuawvcv2e0k80mubg7vx7vx72/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 <script src="init.js"></script>
 <form method="post" action="<?= $action ?>" class="form-group container">
-    <textarea name="editor" id="myeditorinstance" class="form-control"><?php if (isset($data)) {
-                                                                            echo htmlspecialchars($data);
-                                                                        } ?></textarea>
+    <textarea name="editor" id="default-editor" class="form-control"><?php if (isset($data)) {
+        echo htmlspecialchars($data);
+    } ?></textarea>
     <input type="hidden" name="id" value="<?= $id ?>"> <!-- Add this line to include the ID -->
-
+    
     <button type="submit" class="btn btn-primary mt-3">Submit</button>
+    <button type="submit" class="btn btn-danger mt-3" formaction="/custom/delete">Delete</button>
 </form>
--- app/views/wysiwyg/init.js
+++ app/views/wysiwyg/init.js
@@ -1,15 +1,3 @@
 tinymce.init({
-  selector: '#myeditorinstance',
-  height: 500,  // Change the height of the editor
-  menubar: true,  // Enable the menubar
-  plugins: [  // Enable plugins
-    'advlist autolink lists link image charmap print preview anchor',
-    'searchreplace visualblocks code fullscreen',
-    'insertdatetime media table paste code help wordcount'
-  ],
-  toolbar: 'undo redo | formatselect | ' +
-  'bold italic backcolor | alignleft aligncenter ' +
-  'alignright alignjustify | bullist numlist outdent indent | ' +
-  'removeformat | help',  // Customize the toolbar
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'  // Set content style
+  selector: '#default-editor',
 });
