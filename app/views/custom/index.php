352ffc09218f2ee2933c869ff82cdd1483c8f4e8
Luke Huisman <huisman.l@yahoo.com>
2024-03-13 08:16:07 +0100

    Why no work

--- app/views/custom/index.php
+++ app/views/custom/index.php
@@ -6,7 +6,12 @@ if (isset($customPage)) {
     if (!isset($_SESSION['user']) or $_SESSION['user']['role'] == 'admin') {
         $action = '/custom/update';
         $data = $customPage->getContent();
-        require_once __DIR__ . '/../wysiwyg/index.php';
+?>
+        <div class="container">
+
+            <?php require_once __DIR__ . '/../wysiwyg/index.php'; ?>
+        </div>
+<?php
     }
 } else {
     require_once __DIR__ . '/../404.php';
--- app/views/wysiwyg/index.php
+++ app/views/wysiwyg/index.php
@@ -1,6 +1,7 @@
 <script src="https://cdn.tiny.cloud/1/30x8vgbwun6m4b3yb3zv85ytuawvcv2e0k80mubg7vx7vx72/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
-<script src="init.js"></script>
-<form method="post" action="<?= $action ?>" class="form-group container">
+<script src="/js/init.js"></script>
+<form method="post" action="<?= $action ?>" class="form-group mt-5">
+    <label for="default-editor"></label>
     <textarea name="editor" id="default-editor" class="form-control"><?php if (isset($data)) {
         echo htmlspecialchars($data);
     } ?></textarea>
