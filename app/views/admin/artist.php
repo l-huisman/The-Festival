6bfe5cab7444bfdbb7efeb42b733a90a36c71691
Luke Huisman <huisman.l@yahoo.com>
2024-03-21 14:10:59 +0100

    Refactor admin views to include artist edit functionality

--- app/views/admin/artist.php
+++ app/views/admin/artist.php
@@ -2,9 +2,20 @@
 require_once __DIR__ . '/../elements/header.php';
 ?>
 
+<style>
+    td {
+        vertical-align: middle;
+    }
+
+    input[type="text"] {
+        width: 100%;
+        border: none;
+    }
+</style>
+
 <div class="container mt-2">
     <h2>Artists</h2>
-    <p>Press on the fields to change the values within</p>
+    <p>Press on the fields to change the values within and press edit to update the fields</p>
     <table class="table">
         <thead>
             <tr>
@@ -18,30 +29,33 @@ require_once __DIR__ . '/../elements/header.php';
         <tbody>
             <?php foreach ($artists as $artist) { ?>
                 <tr>
-                    <td contenteditable="true"><?php echo $artist->getName(); ?></td>
-                    <td contenteditable="true"><?php echo $artist->getDescription(); ?></td>
-                    <td>
-                        <form action="upload.php" method="post" enctype="multipart/form-data">
-                            <label for="banner">
-                                <img src="<?php echo $artist->getBanner(); ?>" alt="Banner" style="width: 100px; height: 100px;">
-                            </label>
-                            <input type="file" id="banner" name="banner" style="display: none;">
-                            <input type="submit" value="Upload Image" name="submit">
-                        </form>
-                    </td>
-                    <td>
-                        <form action="upload.php" method="post" enctype="multipart/form-data">
-                            <label for="pictogram">
-                                <img src="<?php echo $artist->getPictogram(); ?>" alt="Pictogram" style="width: 100px; height: 100px;">
-                            </label>
-                            <input type="file" id="pictogram" name="pictogram" style="display: none;">
-                            <input type="submit" value="Upload Image" name="submit">
-                        </form>
-                    </td>
-                    <td>
-                        <a href="/admin/artist/edit?id=<?php echo $artist->getId(); ?>" class="btn btn-primary">Edit</a>
-                        <a href="/admin/artist/delete?id=<?php echo $artist->getId(); ?>" class="btn btn-danger">Delete</a>
-                    </td>
+                    <form action="/admin/updateArtist" method="post">
+                        <td><input type="text" name="name" value=<?php echo $artist->getName(); ?>></td>
+                        <td><input type="text" name="description" value=<?php echo $artist->getDescription(); ?>></td>
+                        <td>
+                            <form action="upload.php" method="post" enctype="multipart/form-data">
+                                <label for="banner">
+                                    <img src="<?php echo $artist->getBanner(); ?>" alt="Banner" style="width: 100px; height: 100px;">
+                                </label>
+                                <input type="file" id="banner" name="banner" style="display: none;">
+                                <input type="submit" value="Upload Image" name="submit">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="upload.php" method="post" enctype="multipart/form-data">
+                                <label for="pictogram">
+                                    <img src="<?php echo $artist->getPictogram(); ?>" alt="Pictogram" style="width: 100px; height: 100px;">
+                                </label>
+                                <input type="file" id="pictogram" name="pictogram" style="display: none;">
+                                <input type="submit" value="Upload Image" name="submit">
+                            </form>
+                        </td>
+                        <td>
+                            <input type="hidden" name="id" value=<?php echo $artist->getId(); ?>>
+                            <a href="/admin/admin/updateArtist" class="btn btn-primary">Edit</a>
+                            <a href="/admin/admin/deleteArtist" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete this artist?')">Delete</a>
+                        </td>
+                    </form>
                 </tr>
             <?php } ?>
         </tbody>
