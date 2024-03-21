be37a76aa09119792dbdc97812ada9165ddcfce3
Luke Huisman <huisman.l@yahoo.com>
2024-03-21 10:33:45 +0100

    Refactor adminController.php for improved readability and maintainability

--- app/controllers/adminController.php
+++ app/controllers/adminController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace Controllers;
 
-class AdminController{
+class AdminController
+{
     private $adminService;
     public function __construct()
     {
@@ -9,71 +11,71 @@ class AdminController{
 
         $url = explode('/', $_SERVER['REQUEST_URI']);
         $method = $_SERVER['REQUEST_METHOD'];
-        
-        if($url[2] === 'loginPage' || $url[2] === 'login' && $method == 'POST'){
+
+        if ($url[2] === 'loginPage' || $url[2] === 'login' && $method == 'POST') {
             return;
         }
-        if(isset($_SESSION['user'])){
-            $user = unserialize($_SESSION['user']);
-            if($user->role != "admin"){
-                header('Location:/');
-            }  
-        }else {
+        $this->is_admin();
+    }
+
+    public function is_admin()
+    {
+        if (!isset($_SESSION['user']) || unserialize($_SESSION['user'])->role == "admin") {
             header('Location:/');
+            die();
         }
     }
 
-    public function login(){
+    public function login()
+    {
         $email = htmlspecialchars($_POST['email']);
         $password = htmlspecialchars($_POST['password']);
         $this->adminService->verifyAdminLogin($email, $password);
     }
 
-    public function overviewUsers(){
-        if(isset($_SESSION['user'])){
-            $user = unserialize($_SESSION['user']);
-            if($user->role == "admin"){
-                $users = $this->adminService->getAllUsers();
-                require_once __DIR__ . '/../views/user/overviewUsers.php';
-            }
-            else {
-                header('Location:/');
-                die();
-            }
-        }else {
-            header('Location:/testtest');
-            die();
-        }
+    public function overviewUsers()
+    {
+        $users = $this->adminService->getAllUsers();
+        require_once __DIR__ . '/../views/user/overviewUsers.php';
     }
 
-    public function deleteUser(){
-        if(isset($_SESSION['user']) ){
+    public function music()
+    {
+        $music = $this->adminService->getAllMusic();
+        require_once __DIR__ . '/../views/music/music.php';
+    }
+
+
+    public function deleteUser()
+    {
+        if (isset($_SESSION['user'])) {
             $user = unserialize($_SESSION['user']);
-            if($user->role == 'admin'){
+            if ($user->role == 'admin') {
                 $user_id = htmlspecialchars($_GET['user_id']);
                 $this->adminService->deleteUser($user_id);
                 header('Location:/admin/overviewUsers');
-            }
-            else {
+            } else {
                 header('Location:/');
                 die();
             }
-        }else {
+        } else {
             header('Location:/');
             die();
         }
     }
 
-    public function editUserView(){
+    public function editUserView()
+    {
         $user_id = htmlspecialchars($_GET['user_id']);
         $user = $this->adminService->getUserById($user_id);
         require_once __DIR__ . '/../views/user/editUser.php';
     }
 
-    public function validateUserAccount(){
-        if(isset($_SESSION['user'])){
+    public function validateUserAccount()
+    {
+        if (isset($_SESSION['user'])) {
             $user = unserialize($_SESSION['user']);
-            if($user->role == 'admin'){
+            if ($user->role == 'admin') {
                 $newFirstName = htmlspecialchars($_POST['newFirstName']);
                 $newLastName = htmlspecialchars($_POST['newLastName']);
                 $newDateOfBirth = htmlspecialchars($_POST['newDateOfBirth']);
@@ -84,19 +86,18 @@ class AdminController{
                 $this->adminService->validateUserAccount($newFirstName, $newLastName, $newDateOfBirth, $newAddress, $newPhoneNumber, $newRole, $user_id);
 
                 header('Location:/admin/overviewUsers');
-            }
-            else {
+            } else {
                 header('Location:/');
                 die();
             }
-        }else {
+        } else {
             header('Location:/');
             die();
         }
-        
     }
 
-    public function loginPage(){
+    public function loginPage()
+    {
         require __DIR__ . '/../views/admin/loginPage.php';
     }
 }
