7431a4976bed4c95c23d4d45fe9208b75a4868e5
FejsalA <71178550+FejsalA@users.noreply.github.com>
2024-03-21 12:29:37 +0100

    comments gone

--- app/views/elements/header.php
+++ app/views/elements/header.php
@@ -23,26 +23,29 @@
 
 <body>
     <div class="container">
-        <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom" style="background-color: #ffffff !important;">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="/">H</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarText">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/yummy">Yummy</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/music">Music</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/historic">History</a>
-                        </li>
-                    </ul>
-                    <ul class="navbar-nav">
-                        <?php
+    <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom" style="background-color: #ffffff !important;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">H</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <a class="nav-link" href="/yummy">Yummy</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/music">Music</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/historic">History</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="/contact">Info</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <?php 
 
                         if (isset($_SESSION['user'])) { ?>
                             <li class="nav-item">
