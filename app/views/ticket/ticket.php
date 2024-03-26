291126a913da3466296b783af72c72981a9a7fa2
Yara Bok <yarabok210804@gmail.com>
2024-03-26 20:14:36 +0100

    opzet sendTicketService

---
+++ app/views/ticket/ticket.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+        <h1>Your tickets</h1>
+        <p>Thank you for your purchase. Below you will find your tickets for the Haarlem Festival.</p>
+
+        <p>Name: {name}</p>
+        <p>Event: {event}</p>
+        <p>Date and time: {date + time}</p>
+        <p>Location: {location}</p>
+
+        <img>{qrCode}</img>
+        
+    </body>
+</html>
